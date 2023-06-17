fd94da44a3b9986133b45cf650efac332248516d
marcuses101 <mnjconnolly@gmail.com>
2023-06-16 22:47:27 -0400

    feat: add format on save configuration

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -54,6 +54,16 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
+lsp.format_on_save({
+  format_opts = {
+    async = false,
+    timeout_ms = 10000,
+  },
+  servers = {
+    ['rust_analyzer'] = {'rust'},
+  }
+})
+
 lsp.setup()
 
 vim.diagnostic.config({
