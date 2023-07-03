60fcbc7c8e5d1551aec635e850cdfbf39643c3bb
Marcus Connolly <mconnolly@fixnetwork.com>
2023-07-03 15:11:17 -0400

    feat: powershell config

--- lua/marcus/init.lua
+++ lua/marcus/init.lua
@@ -1,3 +1,2 @@
 require("marcus.remap")
 require("marcus.set")
-
--- lua/marcus/packer.lua
+++ lua/marcus/packer.lua
@@ -31,14 +31,21 @@ return require('packer').startup(function(use)
 	      run = function()
 		pcall(vim.cmd, 'MasonUpdate')
 	      end,
-	    },
+	     },
 	    {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
 	    -- Autocompletion
 	    {'hrsh7th/nvim-cmp'},     -- Required
 	    {'hrsh7th/cmp-nvim-lsp'}, -- Required
 	    {'L3MON4D3/LuaSnip'},     -- Required
-	}
+	},
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+    }
+
 }
 
-end)				
+end)
